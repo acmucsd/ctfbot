20958e2a09aada79660a8ed9df6f1dbced8d599f
KNOXDEV <nick@knox.codes>
2021-01-18 09:22:31 -0800

    varadic logger

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,5 @@
 import { Pool } from 'pg';
+import log from '../log';
 
 import { postgresConfig } from '../config';
 import schemas from './schemas';
@@ -8,11 +9,9 @@ const pool = new Pool(postgresConfig);
 // initialize each table
 schemas.forEach((schema) => {
   pool.query(schema).then().catch((err) => {
-    console.log('unable to initialize table: ', err);
+    log('unable to initialize table: ', err);
     process.exit(1);
   });
 });
 
-export default {
-  query: (text, params, callback) => pool.query(text, params, callback),
-};
+export default (text: string, params, callback) => pool.query(text, params, callback);
