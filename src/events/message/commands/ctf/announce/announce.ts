192bbcf39ce0b1e5fb9f2cd0a07aa42e4b227b62
Andrew Peterson <andypeterson526@gmail.com>
2021-01-24 00:15:47 -0800

    fixed ctf announce

--- src/events/message/commands/ctf/announce/announce.ts
+++ src/events/message/commands/ctf/announce/announce.ts
@@ -2,7 +2,7 @@ import { Message } from 'discord.js';
 import ctf from '../../../../../database/models/ctf';
 
 const announce = async (message: Message, messageToSend: string) => {
-  const CTF:ctf = ctf.fromGuildSnowflake(message.guild.id);
+  const CTF:ctf = await ctf.fromGuildSnowflake(message.guild.id);
   await CTF.setDescription(messageToSend);
 };
 
