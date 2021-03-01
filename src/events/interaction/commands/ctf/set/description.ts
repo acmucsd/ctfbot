abbc4f62efcb698e446d472c51a40dfc6219ade7
KNOXDEV <nick@knox.codes>
2021-03-01 11:43:03 -0800

    ctf set description

--- src/events/interaction/commands/ctf/set/description.ts
+++ src/events/interaction/commands/ctf/set/description.ts
@@ -1,5 +1,6 @@
 import CommandInteraction from '../../../compat/CommandInteraction';
 import { ApplicationCommandDefinition, ApplicationCommandOptionType, CommandOptionMap } from '../../../compat/types';
+import { CTF } from '../../../../../database/models';
 
 export default {
   name: 'description',
@@ -13,7 +14,10 @@ export default {
       required: true,
     },
   ],
-  execute(interaction: CommandInteraction, options: CommandOptionMap) {
+  async execute(interaction: CommandInteraction, options: CommandOptionMap) {
+    const description = options.description.toString() || '';
+    const ctf = await CTF.fromGuildSnowflakeCTF(interaction.guild.id);
+    await ctf.setDescription(description);
     return `this command (${interaction.commandID}) has not been implemented yet`;
   },
 } as ApplicationCommandDefinition;
