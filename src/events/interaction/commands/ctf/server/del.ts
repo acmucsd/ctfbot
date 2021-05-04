4b2e9ef19117fcfd5f52da84bb4b937d3d374231
KNOXDEV <nick@knox.codes>
2021-05-04 06:12:51 -0700

    ctf del await

--- src/events/interaction/commands/ctf/server/del.ts
+++ src/events/interaction/commands/ctf/server/del.ts
@@ -34,7 +34,7 @@ export default {
       ctf = await CTF.fromGuildSnowflakeCTF(interaction.guild.id);
     }
     ctf.throwErrorUnlessAdmin(interaction);
-    void teamServer.deleteTeamServer(interaction.client);
+    await teamServer.deleteTeamServer(interaction.client);
     return `Removed **${teamServer.row.name}** from CTf **${ctf.row.name}**`;
   },
 } as ApplicationCommandDefinition;
