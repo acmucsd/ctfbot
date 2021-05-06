932903c873c43779e557ddb95de899ce0d15fd09
KNOXDEV <nick@knox.codes>
2021-05-06 10:40:11 -0700

    NoUserError message update

--- src/errors/NoUserError.ts
+++ src/errors/NoUserError.ts
@@ -1,7 +1,7 @@
 export class NoUserError extends Error {
   constructor() {
     super();
-    this.message = 'that user is not in this ctf';
+    this.message = 'that user needs to accept Terms of Service first';
     this.name = 'NoUserError';
 
     if (Error.captureStackTrace) {
