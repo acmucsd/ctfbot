d1eba5e3d6950ed39db02bdfe9bdc890bcbefed8
KNOXDEV <nick@knox.codes>
2022-01-16 21:30:22 -0800

    commented out old database code so the new stuff would execute

--- src/database/database.ts
+++ src/database/database.ts
@@ -7,12 +7,12 @@ import { logger } from '../log';
 const pool = new Pool(postgresConfig);
 
 // initialize each table
-initTables(pool)
-  .then(() => logger.info('tables initialized'))
-  .catch((err) => {
-    logger.info('database initialization failed', err);
-    process.exit(1);
-  });
+// initTables(pool)
+//   .then(() => logger.info('tables initialized'))
+//   .catch((err) => {
+//     logger.info('database initialization failed', err);
+//     process.exit(1);
+//   });
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async (text: string, params?: any[]) => pool.query(text, params);
