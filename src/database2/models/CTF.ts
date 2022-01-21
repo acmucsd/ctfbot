6cba55bf60bb3bd25006eec81c67acf74d5093ac
KNOXDEV <nick@knox.codes>
2022-01-21 08:58:56 -0800

    lol whoops

--- src/database2/models/CTF.ts
+++ src/database2/models/CTF.ts
@@ -94,26 +94,32 @@ export function initCTF(sequelize: Sequelize) {
       endDate: DataTypes.DATE,
       adminRoleSnowflake: {
         type: DataTypes.STRING,
+        defaultValue: '',
         allowNull: false,
       },
       participantRoleSnowflake: {
         type: DataTypes.STRING,
+        defaultValue: '',
         allowNull: false,
       },
       announcementsChannelSnowflake: {
         type: DataTypes.STRING,
+        defaultValue: '',
         allowNull: false,
       },
       scoreboardChannelSnowflake: {
         type: DataTypes.STRING,
+        defaultValue: '',
         allowNull: false,
       },
       tosChannelSnowflake: {
         type: DataTypes.STRING,
+        defaultValue: '',
         allowNull: false,
       },
       infoCategorySnowflake: {
         type: DataTypes.STRING,
+        defaultValue: '',
         allowNull: false,
       },
     },
