d7e30f63a80f22bfe18182cd517746cf64e609fb
KNOXDEV <nick@knox.codes>
2022-04-28 15:26:32 -0700

    /setname

--- src/discord/events/interaction/commands/setname.ts
+++ src/discord/events/interaction/commands/setname.ts
@@ -1,5 +1,6 @@
 import { ApplicationCommandOptionTypes } from 'discord.js/typings/enums';
 import { ChatInputCommandDefinition, PopulatedCommandInteraction } from '../interaction';
+import { getCtfByGuildContext } from '../../../util/ResourceManager';
 
 export default {
   name: 'setname',
@@ -13,7 +14,17 @@ export default {
       required: true,
     },
   ],
-  execute(interaction: PopulatedCommandInteraction) {
-    return 'to be implemented';
+  async execute(interaction: PopulatedCommandInteraction) {
+    const ctf = await getCtfByGuildContext(interaction.guild);
+    if (!ctf) throw new Error('this guild does not belong to a ctf');
+    // check to make sure user is a part of this ctf
+    const { team } = await ctf.getTeamAndUserFromSnowflake(interaction.user.id);
+
+    team.name = interaction.options.getString('name', true);
+    await team.save();
+
+    // trigger leaderboard refresh? nah
+
+    return `Your team has been renamed ${team.name}`;
   },
 } as ChatInputCommandDefinition;
