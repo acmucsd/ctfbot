1439c672dacef0cfb9d4fa3d45d9a21677ec4ee5
KNOXDEV <nick@knox.codes>
2022-05-04 16:56:20 -0700

    no more sql logging in prod please

--- src/config/postgres.ts
+++ src/config/postgres.ts
@@ -1,9 +1,13 @@
+import { logger } from '../log';
+
 const postgres = {
   username: process.env.PGUSER || 'ctfbot',
   host: process.env.PGHOST || 'localhost',
   database: process.env.PGDATABASE || 'ctfbot',
   password: process.env.PGPASSWORD || 'dAf1bjOwYUrVse8DsAZZBh2fkxXQwAbGrmE7EHUA',
   port: Number(process.env.PGPORT) || 5432,
+  // in production, don't log the sql queries
+  logging: process.env.NODE_ENV === 'production' ? false : (msg: string) => logger.debug(msg),
 };
 
 export default postgres;
